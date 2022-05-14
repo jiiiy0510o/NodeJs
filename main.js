cbb787d40c3282d1ec82d84ce6684015cae617b2
Young <jiiiy0510o@gmail.com>
2022-05-14 23:53:13 +0900

    폴더 목록 읽기

--- main.js
+++ main.js
@@ -5,16 +5,63 @@ var url = require("url");
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = new URL("http://localhost:3000" + _url).searchParams;
-  console.log(queryData.get("id"));
-  if (_url == "/") {
-    _url = "/index.html";
+
+  if (queryData === "/") {
+    if (title === null) {
+      fs.readFile(`data/${queryData.get("id")}`, "utf8", function (err, description) {
+        var title = "Welcome";
+        var description = "Hello, Node.js";
+        var template = `
+      <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="index.html">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+      `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else {
+      fs.readFile(`data/${queryData.get("id")}`, "utf8", function (err, description) {
+        var title = queryData.get("id");
+        var template = `
+    <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="index.html">WEB</a></h1>
+        <ol>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ol>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+    `;
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+  } else {
+    response.writeHead(200);
+    response.end("Not found");
   }
-  if (_url == "/favicon.ico") {
-    response.writeHead(404);
-    response.end();
-    return;
-  }
-  response.writeHead(200);
-  response.end(queryData.id);
 });
 app.listen(3000);
