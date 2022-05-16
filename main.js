8f6b7bd7738a56bbce47d84f4379e74778159c7f
Young <jiiiy0510o@gmail.com>
2022-05-16 23:57:34 +0900

    Not found 오류 구현

--- main.js
+++ main.js
@@ -4,63 +4,42 @@ var url = require("url");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
-  var queryData = new URL("http://localhost:3000" + _url).searchParams;
+  var myUrl = new URL("http://localhost:3000" + _url);
+  var queryData = myUrl.searchParams;
+  var title = queryData.get("id");
+  var pathname = myUrl.pathname;
 
-  if (queryData === "/") {
-    if (title === null) {
-      fs.readFile(`data/${queryData.get("id")}`, "utf8", function (err, description) {
-        var title = "Welcome";
-        var description = "Hello, Node.js";
-        var template = `
-      <!doctype html>
-        <html>
+  console.log(title);
+  console.log(myUrl);
+
+  if (pathname === "/") {
+    fs.readFile(`data/${queryData.get("id")}`, "utf8", function (err, description) {
+      var template = `
+      <!DOCTYPE html>
+      <html>
         <head>
           <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
+          <meta charset="utf-8" />
         </head>
         <body>
-          <h1><a href="index.html">WEB</a></h1>
-          <ol>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
             <li><a href="/?id=HTML">HTML</a></li>
             <li><a href="/?id=CSS">CSS</a></li>
             <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
+          </ul>
           <h2>${title}</h2>
-          <p>${description}</p>
+          <p>
+            ${description}
+          </p>
         </body>
-        </html>
-      `;
-        response.writeHead(200);
-        response.end(template);
-      });
-    } else {
-      fs.readFile(`data/${queryData.get("id")}`, "utf8", function (err, description) {
-        var title = queryData.get("id");
-        var template = `
-    <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="index.html">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
       </html>
-    `;
-        response.writeHead(200);
-        response.end(template);
-      });
-    }
+      `;
+      response.writeHead(200);
+      response.end(template);
+    });
   } else {
-    response.writeHead(200);
+    response.writeHead(404);
     response.end("Not found");
   }
 });
