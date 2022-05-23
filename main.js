bb49a0dce159741cf2df823f9eb20b3de60ed627
Young <jiiiy0510o@gmail.com>
2022-05-24 01:19:36 +0900

    module 만들기

--- main.js
+++ main.js
@@ -2,34 +2,7 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 
-var template = {
-  html: function (title, list, body, control) {
-    return `  
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>WEB - ${title}</title>
-        <meta charset="utf-8" />
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        ${control}
-        ${body}
-      </body>
-    </html>`;
-  },
-  list: function (filelist) {
-    var list = `<ul>`;
-    var i = 0;
-    while (i < filelist.length) {
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
-    }
-    list = list + "</upm2l>";
-    return list;
-  },
-};
+var template = require("./lib/template.js");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
